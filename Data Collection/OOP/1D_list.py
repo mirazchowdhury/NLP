58584bbb61534da2aa8ae0fc968edfa36a491666
mirazchowdhury <mirazchowdhury03@gmail.com>
2025-02-11 15:00:32 +0600

    1D list added

---
+++ Data Collection/OOP/1D_list.py
@@ -0,0 +1,6 @@
+elements = ["eggs","tomatos","brocollis","potatos","chickens"]
+size_elements = len(elements)
+# for i in elements:
+#     print(i)
+for i in range(size_elements):
+    print(elements[i])
